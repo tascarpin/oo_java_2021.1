c2bc6f5394f09458cf41c214ddf305e5cdd57243
Tassio Pinheiro <tascarpin@yahoo.com.br>
2021-03-30 20:02:08 -0300

    Organizando os branches

--- src/sintaxe/Executa.java
+++ src/sintaxe/Executa.java
@@ -6,51 +6,16 @@ public class Executa {
 		
 		Sintaxe objeto = new Sintaxe();
 		
-//		objeto.calculaInt();
-//		objeto.calculaFloat();
-//		objeto.calculaDouble();
-//		objeto.calculaComCast();
-//		System.out.println(objeto.int1);
-//		objeto.concatenaString();
-//		objeto.vetorChar();
-		
-//		objeto.fluxoFor1();
-//		objeto.fluxoFor2();
-//		objeto.fluxoFor3();
-//		objeto.fluxoFor4();
-//		objeto.fluxoFor5();
-//		objeto.fluxoFor6();
-		
-//		objeto.fluxoForVet();
-//		objeto.fluxoForVet2();
-//		objeto.fluxoForVet3();
-
-
-//		objeto.concatCharComFor1();
-//		objeto.concatCharComFor2();
-//		objeto.concatCharComFor3();
-		
-//		objeto.valueOf();
-//		objeto.codicionalIF1();
-//		objeto.codicionalIF2();
-//		objeto.codicionalIF3();
-		
-//		objeto.fluxoFor();
-//		objeto.concatCharComFor();
-//		objeto.contcatCharComOperador();
-		
-//		objeto.operadorLogico();
-//		objeto.operadorLogico2();
-//		objeto.loopWhile();
-//		objeto.incrementosPrePos();
-		objeto.loopDoWhile();
-		
-		
-
-			
-
-		
-		
+		objeto.calculaInt();
+		objeto.calculaFloat();
+		objeto.calculaDouble();
+		objeto.calculaComCast();
+		System.out.println(objeto.int1);
+		objeto.concatenaString();
+		objeto.vetorChar();
+		objeto.fluxoFor();
+		objeto.concatChar();
+		objeto.contcatCharComOperador();
 	}
 
 }
